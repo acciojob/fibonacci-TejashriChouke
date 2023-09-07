35fc7ba2cb31ef1e12b042ac271257c462b00787
TejashriChouke <139746868+TejashriChouke@users.noreply.github.com>
2023-09-07 23:25:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function fibonacci(num) {
 // your code here
+	if(n==1) return 0;
+		if(n==2) return 1;
+		let prev = fib(n-1);
+		let prev2 = fib(n-2);
+
+		return prev + prev2;
+    }
 }
 
 module.exports = fibonacci;
